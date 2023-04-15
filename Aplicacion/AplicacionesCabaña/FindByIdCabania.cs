bc36633db31c093fd3bc2c0f21f8704462dad5da
Mariano <mariano.escobal@cencosud.com>
2023-04-15 11:49:16 -0300

    commit

--- Aplicacion/AplicacionesCabaña/FindByIdCabania.cs
+++ Aplicacion/AplicacionesCabaña/FindByIdCabania.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Negocio.Entidades;
+using Negocio.InterfacesRepositorio;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,19 @@ using System.Threading.Tasks;
 
 namespace Aplicacion.AplicacionesCabaña
 {
-    internal class FindById
+    public class FindByIdCabania : IFindByIdCabania
+
     {
+        public IRepositorioCabania RepositorioCabania { get; set; }
+
+        public FindByIdCabania(IRepositorioCabania repo) {
+            RepositorioCabania = repo;
+        }
+
+
+        public Cabania FindById(int id)
+        {
+            return RepositorioCabania.FindById(id);
+        }
     }
 }
